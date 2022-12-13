2871be342c4e10528a9c1eb6f1b16912272ad3d4
manhimself26 <rewardmuchech@gmail.com>
2022-12-13 12:07:27 +0000

    shell

--- realloc.c
+++ realloc.c
@@ -17,4 +17,46 @@ char *_memset(char *s, char b, unsigned int n)
 }
 
 /**
- *
+ * ffree - frees a string of strings
+ * @pp: string of strings
+ */
+void ffree(char **pp)
+{
+	char **a = pp;
+
+	if (!pp)
+		return;
+	while (*pp)
+		free(*pp++);
+	free(a);
+}
+
+/**
+ * _realloc - reallocates a block of memory
+ * @ptr: pointer to previous mem allocated block
+ * @old_size: byte size of prev block
+ * @new_size: byte size of new block
+ * Return: pointer
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *p;
+
+	if (!ptr)
+		return (malloc(new_size));
+	if (!new_size)
+		return (free(ptr), NULL);
+	if (new_size == old_size)
+		return (ptr);
+
+	p = malloc(new_size);
+	if (!p)
+		return (NULL);
+
+	old_size = old_size < new_size ? old_size : new_size;
+	while (old_size--)
+		p[old_size] = ((char *)ptr)[old_size];
+	free(ptr);
+	return (p);
+}
+
