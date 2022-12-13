457c2823c7b743519350705cac7e91339798689b
manhimself26 <rewardmuchech@gmail.com>
2022-12-13 03:54:43 -0800

    shell

---
+++ realloc.c
@@ -0,0 +1,20 @@
+#include "shell.h"
+
+/**
+ * _memset - fills memory with a constant byte
+ * @s: pointer to memory area
+ * @b: byte to fill *s with
+ * @n: amount of bytes to be filled
+ * Return: (s) a pointer to memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
+}
+
+/**
+ *
